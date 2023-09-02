fafbbf57c7b896cd478f52b52e59044c9c03a56a
IshanCornick <ishancornick777@gmail.com>
2023-09-02 13:02:12 -0700

    finished goofynames

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,14 +11,27 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String goofy = JOptionPane.showInputDialog("Enter your name");
 		// 2. Print upper case name to the console using .toUpperCase()
+		String upper = goofy.toUpperCase();
+		System.out.println(upper);
 		//    Run your program to see that this works.
-
+	
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
-				// 4. Create a char variable to store the next character of the name
+		for (int i = 0;i < goofy.length();i++) {
+			char amazon = goofy.charAt(i);
+			if (i%2==0) {
+				goofyName += Character.toUpperCase(amazon);
+			}else {
+				goofyName += Character.toLowerCase(amazon);
+			}
+			
+	
+			
+		}
+		JOptionPane.showMessageDialog(null, goofyName);
+		// 4. Create a char variable to store the next character of the name
 				//    use .charAt()
 	
 				
